237b04ed45a22de5b64308adc6a5817f4b5d6306
Khwaja Abdul Rafae <71258088+okrafae@users.noreply.github.com>
2022-10-07 03:13:03 +0530

    Time: 30 ms (39.12%), Space: 13 MB (95.63%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -3,33 +3,29 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int m = matrix.size();
         int n = matrix[0].size();
-        vector<int> row_flag(m,1);
-        vector<int> col_flag(n,1);
+        int colflag = 1;
         for( int i = 0; i < m; i++)
         {
-            for( int j = 0; j < n; j++)
+            if( matrix[i][0] == 0 ) colflag = 0;
+            for( int j = 1; j < n; j++)
             {
-                if( matrix[i][j] == 0 )
+                if( matrix[i][j] == 0)
                 {
-                    row_flag[i] = 0;
-                    col_flag[j] = 0;
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
         
-        for( int i = 0; i < m; i++)
+        for( int i = m - 1; i >= 0; i--)
         {
-            if( row_flag[i] == 0 )
+            for( int j = n - 1; j >= 1; j--)
             {
-                for( int j = 0; j < n; j++) matrix[i][j] = 0;
+                if( matrix[i][0] == 0 || matrix[0][j] == 0) matrix[i][j] = 0;
             }
-        }
-        
-        for( int j = 0; j < n; j++)
-        {
-            if( col_flag[j] == 0 )
+            if( colflag == 0 )
             {
-                for( int i = 0; i < m; i++) matrix[i][j] = 0;
+                matrix[i][0] = 0;
             }
         }
     }
