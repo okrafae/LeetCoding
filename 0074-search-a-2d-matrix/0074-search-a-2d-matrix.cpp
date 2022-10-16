f1caf2d75a21e13dcf84253aa3f4786560dab422
Khwaja Abdul Rafae <71258088+okrafae@users.noreply.github.com>
2022-10-16 14:30:47 +0530

    Time: 7 ms (64.78%), Space: 9.6 MB (51.25%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -3,6 +3,7 @@ public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int m = matrix.size(), n = matrix[0].size();
         int colLow = 0, colHigh = m - 1,colMid =0;
+        //binary searching first column
         while( colLow <= colHigh )
         {
             colMid = (colLow + colHigh ) /2;
@@ -12,9 +13,9 @@ public:
                 colLow = colMid+1;
         }
         int rowLow = 0, rowHigh = n -1,rowMid =0;
-        cout << colLow << " " << colMid << " " << colHigh << endl;
+        //to avoid edge case like [[1]] & target = 0. to search in first row.
         if( colHigh < 0) colHigh++;
-         //return false;
+        //binary searching the row in which it might be present
         while( rowLow <= rowHigh )
         {
             rowMid = ( rowLow + rowHigh ) / 2;
@@ -22,7 +23,6 @@ public:
             else if( target < matrix[colHigh][rowMid]) rowHigh = rowMid - 1;
             else rowLow = rowMid + 1;
         }
-        cout << rowLow << " " << rowMid << " " << rowHigh << endl;
 
         return false;
     }
