df1dfbb29468fea5ee5aaf1877ed2a001c9a5d4a
Khwaja Abdul Rafae <71258088+okrafae@users.noreply.github.com>
2022-10-16 15:05:41 +0530

    Time: 16 ms (7.28%), Space: 9.5 MB (51.25%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,29 +1,15 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int m = matrix.size(), n = matrix[0].size();
-        int colLow = 0, colHigh = m - 1,colMid =0;
-        //binary searching first column
-        while( colLow <= colHigh )
+        int m = matrix.size(), n = matrix[0].size(), low = 0, high = m*n - 1;
+        while( low <= high)
         {
-            colMid = (colLow + colHigh ) /2;
-            if( target == matrix[colMid][0] ) return true;
-            else if( target < matrix[colMid][0] ) colHigh = colMid - 1;
-            else
-                colLow = colMid+1;
+            int mid = (low + high)/2;
+            int rowIndex = mid/n, colIndex = mid%n;
+            if( matrix[rowIndex][colIndex] < target) low = mid + 1;
+            else if( matrix[rowIndex][colIndex] > target) high = mid - 1;
+            else return true;
         }
-        int rowLow = 0, rowHigh = n -1,rowMid =0;
-        //to avoid edge case like [[1]] & target = 0. to search in first row.
-        if( colHigh < 0) colHigh++;
-        //binary searching the row in which it might be present
-        while( rowLow <= rowHigh )
-        {
-            rowMid = ( rowLow + rowHigh ) / 2;
-            if( target == matrix[colHigh][rowMid]) return true;
-            else if( target < matrix[colHigh][rowMid]) rowHigh = rowMid - 1;
-            else rowLow = rowMid + 1;
-        }
-
         return false;
     }
 };
