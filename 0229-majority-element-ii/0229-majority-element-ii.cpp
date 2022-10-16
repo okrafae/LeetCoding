33c8e4dda02cf84548394b5b74e812eb2167d426
Khwaja Abdul Rafae <71258088+okrafae@users.noreply.github.com>
2022-10-16 16:28:31 +0530

    Time: 39 ms (9.98%), Space: 16 MB (5.26%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -2,17 +2,35 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         int n = nums.size();
-        unordered_map<int,int> m;
+        int mostFrequentElement = INT_MIN, secondMostFrequentElement = INT_MIN, countOfMostFrequentElement = 0, countOfSecondMostFrequentElement = 0;
         vector<int> ans;
         for( int i = 0; i < n; i++)
         {
-            m[nums[i]]++;
-            if( m[nums[i]] > n/3 ) 
+            if( nums[i] == mostFrequentElement) countOfMostFrequentElement++;
+            else if( nums[i] == secondMostFrequentElement ) countOfSecondMostFrequentElement++;
+            else if( countOfMostFrequentElement == 0 ) 
             {
-                m[nums[i]] = INT_MIN;
-                ans.push_back(nums[i]);
+                mostFrequentElement = nums[i];
+                countOfMostFrequentElement++;
+            }
+            else if( countOfSecondMostFrequentElement == 0){
+                secondMostFrequentElement = nums[i];
+                countOfSecondMostFrequentElement++;
+            }
+            else
+            {
+                countOfMostFrequentElement--;
+                countOfSecondMostFrequentElement--;
             }
         }
+        countOfMostFrequentElement = 0, countOfSecondMostFrequentElement = 0;
+        for( auto i : nums)
+        {
+            if( i == mostFrequentElement ) countOfMostFrequentElement++;
+            if( i == secondMostFrequentElement ) countOfSecondMostFrequentElement++;
+        }
+        if( countOfMostFrequentElement > n/3 ) ans.push_back(mostFrequentElement);
+        if( countOfSecondMostFrequentElement > n/3) ans.push_back(secondMostFrequentElement);
         return ans;
     }
 };
