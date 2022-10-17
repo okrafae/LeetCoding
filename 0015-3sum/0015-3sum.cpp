da43592b9ace0a7e01c2a2171e3a48d0df2a57e2
Khwaja Abdul Rafae <71258088+okrafae@users.noreply.github.com>
2022-10-17 23:59:50 +0530

    Time: 2662 ms (5.02%), Space: 185.1 MB (6.33%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -4,9 +4,10 @@ public:
         vector<vector<int>> ans;
         sort( nums.begin(), nums.end() );
         int n = nums.size();
+        set<vector<int>> s;
         for( int i = 0; i < n - 2; i++)
         {
-            if( i > 0 && nums[i] == nums[i-1] ) continue;
+            //if( i > 0 && nums[i] == nums[i-1] ) continue;
             int j = i + 1, k = n - 1;
             while( j < k )
             {
@@ -15,13 +16,17 @@ public:
                 else
                 {
                     vector<int> temp = {nums[i],nums[j],nums[k]};
-                    ans.push_back(temp);
-                    int previous_j = j, previous_k = k;
-                    while( j < k && nums[previous_j] == nums[j] ) j++;
-                    while( j < k && nums[previous_k] == nums[k] ) k--;
+                    s.insert(temp);
+                    j++;
+                    k--;
+                //    int previous_j = j, previous_k = k;
+              //      while( j < k && nums[previous_j] == nums[j] ) j++;
+                  //  while( j < k && nums[previous_k] == nums[k] ) k--;
                 }
             }
         }
+        for( auto i : s )
+            ans.push_back(i);
         return ans;
     }
 };
