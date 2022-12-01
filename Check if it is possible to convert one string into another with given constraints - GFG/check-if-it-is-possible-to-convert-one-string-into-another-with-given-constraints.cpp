a41e0f0164891403595420b38b1cc1537e111ca5
Khwaja Abdul Rafae <71258088+okrafae@users.noreply.github.com>
2022-12-02 00:49:06 +0530

    Added solution - LeetHub

---
+++ Check if it is possible to convert one string into another with given constraints - GFG/check-if-it-is-possible-to-convert-one-string-into-another-with-given-constraints.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int isItPossible(string S, string T, int M, int N) {
+        // code here
+        int i = 0, j = 0;
+        while( i < M && j < N )
+        {
+            while( i < M && S[i] == '#' ) i++;
+            while( j < N && T[j] == '#' ) j++;
+
+            if( S[i] != T[j] ) return 0;
+            else
+            {
+                i++;
+                j++;
+            }
+        }
+        while( i < M )
+        {
+            if( S[i] == '#' ) i++;
+            else return 0;
+        }
+        while( j < N )
+        {
+            if( T[j] == '#' ) j++;
+            else return 0;
+        }
+        return 1;
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string S, T;
+        cin >> S >> T;
+        int M = S.length(), N = T.length();
+        Solution ob;
+        cout << ob.isItPossible(S, T, M, N);
+        cout << "\n";
+    }
+}
+// } Driver Code Ends
