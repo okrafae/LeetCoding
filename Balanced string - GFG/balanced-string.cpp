96a16a55efd019ffa5c1593bbcde81ff3099207b
Khwaja Abdul Rafae <71258088+okrafae@users.noreply.github.com>
2022-12-15 16:10:32 +0530

    Added solution - LeetHub

---
+++ Balanced string - GFG/balanced-string.cpp
@@ -0,0 +1,88 @@
+//{ Driver Code Starts
+// Initial template for c++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function template for c++
+
+class Solution {
+  public:
+    string BalancedString(int N) {
+        // code here
+        string ans = "";
+        string common = "abcdefghijklmnopqrstuvwxyz";
+        for( int i = 0; i < N/26; i++) ans += common;
+        int n = N%26;
+        if( N%2 == 0 )
+        {
+            for( int i = 0; i < n; i++)
+            {
+                if( i < n/2 )
+                {
+                    ans += char(97+i);
+                }
+                else
+                {
+                    ans += char(123 - n + i);
+                }
+            }
+        }
+        else
+        {
+            int sum = 0;
+            while( N > 0)
+            {
+                int rem = N%10;
+                sum += rem;
+                N /= 10;
+            }
+            if( sum%2 == 0 )
+            {
+                for( int i = 0; i < n; i++)
+                {
+                    if( i <= n/2 )
+                    {
+                        ans += char(97+i);
+                    }
+                    else
+                    {
+                        ans += char(123 - n + i);
+                    }
+                }
+            }
+            else
+            {
+                for( int i = 0; i < n; i++)
+                {
+                    if( i < n/2 )
+                    {
+                        ans += char(97+i);
+                    }
+                    else
+                    {
+                        ans += char(123 - n + i);
+                    }
+                }
+            }
+        }
+        return ans;
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        Solution ob;
+        cout << ob.BalancedString(N) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
