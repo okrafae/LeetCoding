ca4db08d7adaac2818a63baee1211f231cd4508c
Khwaja Abdul Rafae <71258088+okrafae@users.noreply.github.com>
2022-12-20 23:05:40 +0530

    Added solution - LeetHub

---
+++ A difference of values and indexes - GFG/a-difference-of-values-and-indexes.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+
+
+// } Driver Code Ends
+class Solution{
+  public:
+    int maxDistance(int arr[], int n) 
+    { 
+        // Complete the function
+        if( n == 1) return 0;
+        int case_1_max = INT_MIN, case_2_max = INT_MIN, case_1_min = INT_MAX, case_2_min = INT_MAX;
+        for( int i = 0; i < n; i++)
+        {
+            case_1_max = max( case_1_max, arr[i] - i);
+            case_1_min = min( case_1_min, arr[i] - i);
+            case_2_max = max( case_2_max, arr[i] + i);
+            case_2_min = min( case_2_min, arr[i] + i);
+        }
+        
+        // cout << " " << case_1_max - case_1_min << " " << case_2_max - case_2_min << " " << case_1_max + case_1_min << " " << case_2_max + case_2_min << endl;
+        return max( case_1_max - case_1_min, case_2_max - case_2_min);
+        // return max({ case_1_max - case_1_min, case_2_max - case_2_min, case_1_max + case_1_min, case_2_max + case_2_min });
+    } 
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+	int T;
+	cin>>T;
+	while(T--){
+	    int n;
+	    cin>>n;
+	    int arr[n];
+	    for(int i = 0; i<n; i++)
+	        cin>>arr[i];
+	    Solution obj;
+	    cout<<obj.maxDistance(arr, n)<<endl;
+	}
+} 
+
+// } Driver Code Ends
